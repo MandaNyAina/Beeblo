85df178d72b156e4f2907c22b2e53f4269d65f39
MandaNyAina <andrianaivomanda00@gmail.com>
2021-02-14 16:03:24 +0300

    new version

--- beeblo-app/src/app/views/categorie/categorie.component.spec.ts
+++ beeblo-app/src/app/views/categorie/categorie.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -9,7 +9,7 @@ describe('CategorieComponent', () => {
   let component: CategorieComponent;
   let fixture: ComponentFixture<CategorieComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CategorieComponent ]
     })
--- beeblo-app/src/app/views/promotion/promotion.component.spec.ts
+++ beeblo-app/src/app/views/promotion/promotion.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -9,7 +9,7 @@ describe('PromotionComponent', () => {
   let component: PromotionComponent;
   let fixture: ComponentFixture<PromotionComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PromotionComponent ]
     })
